c581b889f8f1e21aa0fe89d6d32cafa525cb1113
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-08 21:54:20 +0800

    [A-5] Added Error Tracking During Runtime

--- app_calculator.py
+++ app_calculator.py
@@ -16,33 +16,66 @@ def operation():
     print("\nOPERATION I: ADDITION \nOPERATION II: SUBTRACTION \nOPERATION III: MULTIPLICATION \nOPERATION IV: DIVISION")
     operation_decision = int(input("\n\nPlease type in the following to choose: \n'1' - ADDITION \n'2' - SUBTRACTION \n'3' - MULTIPLICATION \n'4' - DIVISION \nYour choice is: "))
 
+
     if operation_decision == 1:
-        print("C O M M E N C I N G . . . . .  A D D I T I O N  O P E R A T I O N . . . . .")
-        first_digit = float(input("Please enter a digit as your first number: "))
-        second_digit = float(input("Please enter a digit as your second number: "))
-        add_computation = first_digit + second_digit
-        print("The answer is:",add_computation)
+        try:
+            print("C O M M E N C I N G . . . . .  A D D I T I O N  O P E R A T I O N . . . . .")
+            first_digit = float(input("Please enter a digit as your first number: "))
+            second_digit = float(input("Please enter a digit as your second number: "))
+            add_computation = first_digit + second_digit
+            print("The answer is:",add_computation)
+        except:
+            print("\nAn error occured")
+        else:
+            print("\nNo error has occured")
+        finally:
+            print("Program is finished")
+
     
+
     elif operation_decision == 2:
-        print("C O M M E N C I N G . . . . .  S U B T R A C T I O N  O P E R A T I O N . . . . .")
-        first_digit = float(input("Please enter a digit as your first number: "))
-        second_digit = float(input("Please enter a digit as your second number: "))
-        minus_computation = first_digit - second_digit
-        print("The answer is:",minus_computation)
-    
+        try:
+            print("C O M M E N C I N G . . . . .  S U B T R A C T I O N  O P E R A T I O N . . . . .")
+            first_digit = float(input("Please enter a digit as your first number: "))
+            second_digit = float(input("Please enter a digit as your second number: "))
+            minus_computation = first_digit - second_digit
+            print("The answer is:",minus_computation)
+        except:
+            print("\nAn error occured")
+        else:
+            print("\nNo error has occured")
+        finally:
+            print("Program is finished")
+
+
     elif operation_decision == 3:
-        print("C O M M E N C I N G . . . . .  M U L T I P L I C A T I O N  O P E R A T I O N . . . . .")
-        first_digit = float(input("Please enter a digit as your first number: "))
-        second_digit = float(input("Please enter a digit as your second number: "))
-        multiply_computation = first_digit * second_digit
-        print("The answer is:",multiply_computation)
+        try:
+            print("C O M M E N C I N G . . . . .  M U L T I P L I C A T I O N  O P E R A T I O N . . . . .")
+            first_digit = float(input("Please enter a digit as your first number: "))
+            second_digit = float(input("Please enter a digit as your second number: "))
+            multiply_computation = first_digit * second_digit
+            print("The answer is:",multiply_computation)
+        except:
+            print("\nAn error occured")
+        else:
+            print("\nNo error has occured")
+        finally:
+            print("Program is finished")
+
     
     elif operation_decision == 4:
-        print("C O M M E N C I N G . . . . .  D I V I S I O N  O P E R A T I O N . . . . .")
-        first_digit = float(input("Please enter a digit as your first number: "))
-        second_digit = float(input("Please enter a digit as your second number: "))
-        divide_computation = first_digit / second_digit
-        print("The answer is:",divide_computation)
+        try:
+            print("C O M M E N C I N G . . . . .  D I V I S I O N  O P E R A T I O N . . . . .")
+            first_digit = float(input("Please enter a digit as your first number: "))
+            second_digit = float(input("Please enter a digit as your second number: "))
+            divide_computation = first_digit / second_digit
+            print("The answer is:",divide_computation)
+        except:
+            print("\nAn error occured")
+        else:
+            print("\nNo error has occured")
+        finally:
+            print("Program is finished")
 
     else:
         print("")
